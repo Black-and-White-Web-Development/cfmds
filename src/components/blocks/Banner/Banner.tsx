a5f4e27206f061e7ea7b3f74357806536fb52f8c
Chris <chris79gp@outlook.com>
2025-07-08 15:32:07 +0100

    Feat: add styling for `Banner` block

--- src/components/blocks/Banner/Banner.tsx
+++ src/components/blocks/Banner/Banner.tsx
@@ -1,28 +1,34 @@
 import type { BannerBlock } from "@/types/strapi/blocks/banner.types";
 
-// import "./BannerBlock.scss";
+import "./Banner.scss";
 
 interface BannerProps {
 	block: BannerBlock;
 }
 
 const Banner = ({ block }: BannerProps) => {
+	const backgroundImage = block.image && (
+		<img
+			className="banner__image"
+			src={block.image.url}
+			alt={block.image.alternativeText || ""}
+			width={block.image.width}
+			height={block.image.height}
+		/>
+	);
+
 	return (
-		<section className="block banner">
-			<div className="banner__image-container">
-				<img
-					className="banner__image"
-					src={block.image.url}
-					alt={block.image.alternativeText || ""}
-					width={block.image.width}
-					height={block.image.height}
-				/>
-			</div>
-			<div className="banner__content">
-				<h1>{block.heading}</h1>
-				<p>{block.subheading}</p>
-			</div>
-		</section>
+		<>
+			<section className="block banner">
+				{backgroundImage}
+				<div className="banner__content-container fb-col-wrapper">
+					<div className="banner__content">
+						<h1 className="banner__heading">{block.heading}</h1>
+						<p className="banner__subheading">{block.subheading}</p>
+					</div>
+				</div>
+			</section>
+		</>
 	);
 };
 
