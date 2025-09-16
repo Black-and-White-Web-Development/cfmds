25e1e63ad8d50029c6b931e8a9688e30faf62d35
Chris <chris79gp@outlook.com>
2025-09-16 22:32:01 +0100

    Feat: add styling for committee section

--- src/components/Committee/Committee.tsx
+++ src/components/Committee/Committee.tsx
@@ -36,7 +36,6 @@ const Committee = () => {
 		?.map(section => ({
 			id: section.id,
 			title: section.title,
-			email: section.email,
 			leader: section.leader,
 		}))
 		.sort((a, b) => a.title.localeCompare(b.title));
@@ -44,96 +43,95 @@ const Committee = () => {
 	return (
 		<section className="block committee">
 			<h2 className="committee__heading">Committee</h2>
-			<div className="committee__trustees">
-				<h3 className="committee__subheading">Patrons</h3>
-				<ul className="committee__list">
-					{committee.patrons.map(trustee => (
-						<li key={trustee.id} className="committee__list-item">
-							<article className="committee-member">
-								<h4 className="committee-member__heading">
-									{trustee.name}
-									<span className="committee-member__post-nominals">{trustee.postNominals}</span>
-								</h4>
-							</article>
-						</li>
-					))}
-				</ul>
-			</div>
-			<div className="committee__president">
-				<h3 className="committee__subheading">President</h3>
-				<article className="committee-member">
-					<h4 className="committee-member__heading">
-						{committee.president.name}
-						<span className="committee-member__post-nominals">
-							{committee.president.postNominals}
-						</span>
-					</h4>
-				</article>
-			</div>
-			<div className="committee__vice-presidents">
-				<h3 className="committee__subheading">Vice Presidents</h3>
-				<ul className="committee__list">
-					{committee.vicePresidents.map(vps => (
-						<li key={vps.id} className="committee__list-item">
-							<article className="committee-member">
-								<h4 className="committee-member__heading">
-									{vps.name}
-									<span className="committee-member__post-nominals">{vps.postNominals}</span>
-								</h4>
-							</article>
-						</li>
-					))}
-				</ul>
-			</div>
-			<div className="committee__trustees">
-				<h3 className="committee__subheading">Trustees</h3>
-				<ul className="committee__list">
-					{committee.trustees.map(trustee => (
-						<li key={trustee.id} className="committee__list-item">
-							<article className="committee-member">
-								<h4 className="committee-member__heading">
-									{trustee.committeeMember.name}
-									<span className="committee-member__role">{trustee.role}</span>
-								</h4>
-							</article>
-						</li>
-					))}
-				</ul>
-			</div>
-			<div className="committee__officers">
-				<h3 className="committee__subheading">Officers</h3>
-				<ul className="committee__list">
-					{committee.officers.map(officer => (
-						<li key={officer.id} className="committee__list-item">
-							<article className="committee-member">
-								<h4 className="committee-member__heading">
-									{officer.committeeMember.name}
-									<span className="committee-member__role">{officer.role}</span>
-								</h4>
-							</article>
-						</li>
-					))}
-				</ul>
-			</div>
-			<div className="committee__section-leaders">
-				<h3 className="committee__subheading">Section Leaders</h3>
-				<ul className="committee__list">
-					{sectionLeaders.map(section => (
-						<li key={section.id} className="committee__list-item">
-							<article className="committee-member">
-								<h4 className="committee-member__heading">
-									{section.leader.name}
-									<span className="committee-member__post-nominals">
-										{section.leader.postNominals}
-									</span>
-									<span className="committee-member__role">{section.title} Section Leader</span>
-								</h4>
-								<p className="committee-member__email"></p>
-							</article>
-						</li>
-					))}
-				</ul>
-			</div>
+			<ul className="committee__groups">
+				<li className="committee__group committee__group--trustees">
+					<h3 className="committee__subheading">Patrons</h3>
+					<ul className="committee__list">
+						{committee.patrons.map(trustee => (
+							<li key={trustee.id} className="committee__list-item">
+								<article className="committee-member">
+									<p className="committee-member__name">
+										{trustee.name}{" "}
+										<span className="committee-member__post-nominals">{trustee.postNominals}</span>
+									</p>
+								</article>
+							</li>
+						))}
+					</ul>
+				</li>
+				<li className="committee__group committee__group--president">
+					<h3 className="committee__subheading">President</h3>
+					<article className="committee-member">
+						<p className="committee-member__name">
+							{committee.president.name}{" "}
+							<span className="committee-member__post-nominals">
+								{committee.president.postNominals}
+							</span>
+						</p>
+					</article>
+				</li>
+				<li className="committee__group committee__group--vice-presidents">
+					<h3 className="committee__subheading">Vice Presidents</h3>
+					<ul className="committee__list">
+						{committee.vicePresidents.map(vps => (
+							<li key={vps.id} className="committee__list-item">
+								<article className="committee-member">
+									<p className="committee-member__name">
+										{vps.name}{" "}
+										<span className="committee-member__post-nominals">{vps.postNominals}</span>
+									</p>
+								</article>
+							</li>
+						))}
+					</ul>
+				</li>
+				<li className="committee__group committee__group--trustees">
+					<h3 className="committee__subheading">Trustees</h3>
+					<ul className="committee__list committee__list--with-role">
+						{committee.trustees.map(trustee => (
+							<li key={trustee.id} className="committee__list-item">
+								<article className="committee-member">
+									<h4 className="committee-member__role">{trustee.role}</h4>
+									<p className="committee-member__name">
+										{trustee.committeeMember.name} {trustee.committeeMember.postNominals}
+									</p>
+								</article>
+							</li>
+						))}
+					</ul>
+				</li>
+				<li className="committee__group committee__group--officers">
+					<h3 className="committee__subheading">Officers</h3>
+					<ul className="committee__list committee__list--with-role">
+						{committee.officers.map(officer => (
+							<li key={officer.id} className="committee__list-item">
+								<article className="committee-member">
+									<h4 className="committee-member__role">{officer.role}</h4>
+									<p className="committee-member__name">{officer.committeeMember.name}</p>
+								</article>
+							</li>
+						))}
+					</ul>
+				</li>
+				<li className="committee__group committee__group--section-leaders">
+					<h3 className="committee__subheading">Section Leaders</h3>
+					<ul className="committee__list committee__list--with-role">
+						{sectionLeaders.map(section => (
+							<li key={section.id} className="committee__list-item">
+								<article className="committee-member">
+									<h4 className="committee-member__role">{section.title} Section Leader</h4>
+									<p className="committee-member__name">
+										{section.leader.name}{" "}
+										<span className="committee-member__post-nominals">
+											{section.leader.postNominals}
+										</span>
+									</p>
+								</article>
+							</li>
+						))}
+					</ul>
+				</li>
+			</ul>
 		</section>
 	);
 };
