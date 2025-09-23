013b07d17df72a5106a1600f9c2d0d55a373418e
Chris <chris79gp@outlook.com>
2025-09-23 21:42:17 +0100

    Fix: update header logo to prevent layout shift and make visible in dark mode

--- src/components/Navigation/NavLogo/NavLogo.tsx
+++ src/components/Navigation/NavLogo/NavLogo.tsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 import { useBreakpoint } from "@/hooks/useBreakpoint";
 import "./NavLogo.scss";
 
-import logo from "@/assets/cfmds-logo.svg";
+// import logo from "@/assets/cfmds-logo.svg";
+import Logo from "@/assets/cfmds-logo.svg?react";
 
 const NavLogo = () => {
 	const isTablet = useBreakpoint();
@@ -12,7 +13,8 @@ const NavLogo = () => {
 	return (
 		<div className={clsx("nav-logo", { nav__logo: isTablet ? "header__logo" : "nav__logo" })}>
 			<Link className="nav-logo__link" to="/" aria-label="Home">
-				<img className="nav-logo__image" src={logo} alt="CFMDS logo" width="96" />
+				{/* <img className="nav-logo__image" src={logo} alt="CFMDS logo" width="96" height="67.2" /> */}
+				<Logo className="nav-logo__image" />
 			</Link>
 		</div>
 	);
